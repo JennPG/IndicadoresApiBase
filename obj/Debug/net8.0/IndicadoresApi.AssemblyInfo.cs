948ee0fed24509d7241d0c6ae1f6a2509b49a77e
Jennifer <jennifergonzalez1987l@gmail.com>
2025-03-08 17:32:46 -0500

    Se hizo la modificacion de conexión a la DB

--- obj/Debug/net8.0/IndicadoresApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/IndicadoresApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IndicadoresApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c61322aa8e8d9cfbc1dfafaddd6ca8c7e39378c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c456d1986b002628e9d07058a9481daca23482c")]
 [assembly: System.Reflection.AssemblyProductAttribute("IndicadoresApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IndicadoresApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
